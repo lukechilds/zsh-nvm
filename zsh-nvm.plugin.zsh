3080993da00f281c47a63b9b30258b991550e24f
Luke Childs <lukechilds123@gmail.com>
2016-07-08 13:42:00 +0100

    Silently download file and return contents

--- zsh-nvm.plugin.zsh
+++ zsh-nvm.plugin.zsh
@@ -1,3 +1,11 @@
 _zsh_nvm_has() {
   type "$1" > /dev/null 2>&1
 }
+
+_zsh_nvm_get() {
+  if _zsh_nvm_has "curl"; then
+    curl --silent "$1"
+  elif _zsh_nvm_has "wget"; then
+    wget --quiet --output-document - "$1"
+  fi
+}
