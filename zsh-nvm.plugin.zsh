dde14339e72f8d417af8fbed3eb599b8eab7679b
Luke Childs <lukechilds123@gmail.com>
2016-07-08 19:43:09 +0100

    Get latest release tag

--- zsh-nvm.plugin.zsh
+++ zsh-nvm.plugin.zsh
@@ -9,3 +9,9 @@ _zsh_nvm_get() {
     wget --quiet --output-document - "$1"
   fi
 }
+
+_zsh_nvm_latest_release_tag() {
+  _zsh_nvm_get "https://api.github.com/repos/creationix/nvm/releases/latest" |  # Get latest release from GitHub api
+    grep '"tag_name":' |                                                        # Get tag line
+    sed -E 's/.*"([^"]+)".*/\1/'                                                # Pluck JSON value
+}
