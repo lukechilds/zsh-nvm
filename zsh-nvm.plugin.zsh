cade63ed942f27d0134f1b6df50b99de50f9e8f1
Luke Childs <lukechilds123@gmail.com>
2016-08-27 10:18:45 +0100

    Add `NVM_LAZY_LOAD` option

--- zsh-nvm.plugin.zsh
+++ zsh-nvm.plugin.zsh
@@ -116,5 +116,9 @@ _zsh_nvm_revert() {
 # Install nvm if it isn't already installed
 [[ ! -f "$NVM_DIR/nvm.sh" ]] && _zsh_nvm_install
 
-# If nvm is installed, load it
-[[ -f "$NVM_DIR/nvm.sh" ]] && _zsh_nvm_load
+# If nvm is installed
+if [[ -f "$NVM_DIR/nvm.sh" ]]; then
+
+  # Load it
+  [[ "$NVM_LAZY_LOAD" == true ]] && _zsh_nvm_lazy_load || _zsh_nvm_load
+fi
