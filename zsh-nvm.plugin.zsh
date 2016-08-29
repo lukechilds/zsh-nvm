49f23771d3846e52717df86c16f8efefa7d6a301
Luke Childs <lukechilds123@gmail.com>
2016-08-29 11:52:57 +0100

    Use echo to list glob matches rather than ls ls falls back to current dir if there are no glob matches i.e right after install

--- zsh-nvm.plugin.zsh
+++ zsh-nvm.plugin.zsh
@@ -23,7 +23,7 @@ _zsh_nvm_install() {
 }
 
 _zsh_nvm_global_binaries() {
-  ls "$NVM_DIR"/v0*/bin/*(N) "$NVM_DIR"/versions/*/*/bin/*(N) |
+  echo "$NVM_DIR"/v0*/bin/*(N) "$NVM_DIR"/versions/*/*/bin/*(N) |
     xargs basename |
     sort |
     uniq
