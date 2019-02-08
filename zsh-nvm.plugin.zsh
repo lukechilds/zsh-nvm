9ae1115e76a7ff1e8fcb42e530c196834609f76d
Bartosz Legięć <bartosz@legiec.eu>
2019-02-08 13:30:01 +0100

    Make `nvm upgrade` follow the official git upgrade instructions from nvm (#46)
    https://github.com/creationix/nvm#manual-upgrade

--- zsh-nvm.plugin.zsh
+++ zsh-nvm.plugin.zsh
@@ -13,7 +13,7 @@ _zsh_nvm_has() {
 }
 
 _zsh_nvm_latest_release_tag() {
-  echo $(cd "$NVM_DIR" && git fetch --quiet origin && git describe --abbrev=0 --tags --match "v[0-9]*" origin)
+  echo $(cd "$NVM_DIR" && git fetch --quiet --tags origin && git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1))
 }
 
 _zsh_nvm_install() {
